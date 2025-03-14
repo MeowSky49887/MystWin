7d50df9f468a8d179df0d19393361b07457d2966
Jirat Jaturanpinyo <143077173+MeowSky49887@users.noreply.github.com>
2025-03-15 00:57:23 +0700

    Update bindings.cc

--- src/bindings.cc
+++ src/bindings.cc
@@ -8,11 +8,11 @@ Napi::Object Init(Napi::Env env, Napi::Object exports) {
 	exports.Set(Napi::String::New(env, "detachWindow"), Napi::Function::New(env, mystwin::DetachWindowExport));
 	exports.Set(Napi::String::New(env, "sendToBack"), Napi::Function::New(env, mystwin::SendToBackExport));
 	exports.Set(Napi::String::New(env, "bringToFront"), Napi::Function::New(env, mystwin::BringToFrontExport));
-	exports.Set(Napi::String::New(env, "moveWindow"), Napi::Function::New(env, mystwin::BringToFrontExport));
-	exports.Set(Napi::String::New(env, "resizeWindow"), Napi::Function::New(env, mystwin::BringToFrontExport));
-	exports.Set(Napi::String::New(env, "setOpacity"), Napi::Function::New(env, mystwin::BringToFrontExport));
-	exports.Set(Napi::String::New(env, "toggleOverlay"), Napi::Function::New(env, mystwin::BringToFrontExport));
-	exports.Set(Napi::String::New(env, "toggleFrame"), Napi::Function::New(env, mystwin::BringToFrontExport));
+	exports.Set(Napi::String::New(env, "moveWindow"), Napi::Function::New(env, mystwin::MoveWindowExport));
+	exports.Set(Napi::String::New(env, "resizeWindow"), Napi::Function::New(env, mystwin::ResizeWindowExport));
+	exports.Set(Napi::String::New(env, "setOpacity"), Napi::Function::New(env, mystwin::SetOpacityExport));
+	exports.Set(Napi::String::New(env, "toggleOverlay"), Napi::Function::New(env, mystwin::ToggleOverlayExport));
+	exports.Set(Napi::String::New(env, "toggleFrame"), Napi::Function::New(env, mystwin::ToggleFrameExport));
 	return exports;
 }
 
